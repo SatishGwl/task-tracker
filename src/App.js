25d4abcb64e9201bbe3f99a1a9118f12fe11a3ce
SatishGwl <satish.srcem@gmail.com>
2021-12-18 00:15:43 +0530

    add task to the server

--- src/App.js
+++ src/App.js
@@ -27,12 +27,20 @@ function App() {
 
 //Add Task
 
-const addTask = (task)=> {
-  const id = Math.floor(Math.random() * 1000) + 1
-  console.log(id)
+const addTask = async (task) => {
+const res = await fetch('http://localhost:5000/tasks', {
+  method:'POST',
+  headers: {
+    'Content-type': 'application/json'
+  },
+  body: JSON.stringify(task)
+})
+const data = await res.json()
+setTasks([...tasks, data])
 
-  const newTask = { id, ...task }
-  setTasks([...tasks, newTask])
+  // const id = Math.floor(Math.random() * 1000) + 1
+  // const newTask = { id, ...task }
+  // setTasks([...tasks, newTask])
 }
 
 // Delete Task Function
